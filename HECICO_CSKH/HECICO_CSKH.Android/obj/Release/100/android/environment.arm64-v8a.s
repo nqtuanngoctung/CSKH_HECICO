0ab009b79977e98fccfc07879bda7ce8293d9a4e
QUOC TUAN <quoctuan143@gmail.com>
2021-03-31 17:43:13 +0700

    update ngay 31

--- HECICO_CSKH/HECICO_CSKH.Android/obj/Release/100/android/environment.arm64-v8a.s
+++ HECICO_CSKH/HECICO_CSKH.Android/obj/Release/100/android/environment.arm64-v8a.s
@@ -73,7 +73,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"066e37b4-4076-4f7d-bee5-904da4396f39"
+	.asciz	"43044986-a941-49ce-a9de-dc62979765d4"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
